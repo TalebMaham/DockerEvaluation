8bde96f93b798d8c473b37b9117ce1de42fb19bd
Sidi <sidi.mohamed.taleb@cashflowpositif.fr>
2023-01-27 16:40:58 +0100

    exercice 3

--- docker-evaluation-master/run.sh
+++ docker-evaluation-master/run.sh
@@ -1,7 +1,7 @@
 
-docker run -p 8080:8080 -e SPECILITE=1 worker
-docker run -p 8081:8081  -e SPECILITE=2 worker
-docker run -p 8081:8081  -e SPECILITE=1 worker
-docker run -p 8082:8082  -e SPECILITE=2 worker
+docker run -p 8080:8080 -e SPECIALITE=1 worker
+docker run -p 8081:8081  -e SPECIALITE=2 worker
+docker run -p 8081:8081  -e SPECIALITE=1 worker
+docker run -p 8082:8082  -e SPECIALITE=2 worker
 docker run -p 3000:8080 planner
 
