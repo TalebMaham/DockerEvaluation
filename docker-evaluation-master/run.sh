55466bf05bf56400975d42db4a2d94a65e00ed64
Sidi <sidi.mohamed.taleb@cashflowpositif.fr>
2023-01-27 16:56:02 +0100

    exercice 5

--- docker-evaluation-master/run.sh
+++ docker-evaluation-master/run.sh
@@ -1,6 +1,15 @@
 for i in {1..10}
 do
-  docker run -p 808{$i}:808{$i} -e  worker
+  docker run -p 808{$i}:808{$i} worker
+done
+for i in {1..10}
+do
+  docker run -p 303{$i}:303{$i} -e SPECIALITE=2 worker
+done
+
+for i in {1..10}
+do
+  docker run -p 304{$i}:304{$i}  -e SPECIALITE=1 worker
 done
 docker run -p 3000:3000 planner
 
